3aa2a9c597263f6ce919fd405afab7f212a11fdf
Offex86 <factblink514@gmail.com>
2024-11-04 21:04:05 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,6 +1,6 @@
 // src/worker.js
 import { connect } from "cloudflare:sockets";
-let password = 'auto';
+let password = 'deepu';
 
 //Find proxyIP : https://github.com/NiREvil/vless/blob/main/sub/ProxyIP.md
 let proxyIP = ['bpb.radically.pro'];
